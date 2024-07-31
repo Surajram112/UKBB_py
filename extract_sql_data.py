755d3a2a4da61e8a04379c4d2a77d26ff6548efb
Suraj Ramchand <39829136+Surajram112@users.noreply.github.com>
2024-07-31 13:12:19 +0100

    Update extract_sql_data.py

--- extract_sql_data.py
+++ extract_sql_data.py
@@ -26,8 +26,28 @@ def load_dataset():
     
     return dxdata.load_dataset(id=dispensed_dataset_id)
 
-# Returns all field objects for a given UKB showcase field id
+def read_traits_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
+    # remove any newline characters from each line
+    lines = [line.rstrip('\n') for line in lines]
+
+    # split each line into a list of values
+    lines = [line.split('\t') for line in lines]
+
+    # the first line contains the column names
+    column_names = lines[0]
+
+    # the rest of the lines contain the data
+    data = lines[1:]
+
+    # create a DataFrame from the data
+    df = pd.DataFrame(data, columns=column_names)
+
+    return df
+
+# Returns all field objects for a given UKB showcase field id
 def fields_for_id(field_id, dataset):
     from distutils.version import LooseVersion
     field_id = str(field_id)
@@ -35,23 +55,19 @@ def fields_for_id(field_id, dataset):
     return sorted(fields, key=lambda f: LooseVersion(f.name))
 
 # Returns all field names for a given UKB showcase field id
-
 def field_names_for_id(field_id, dataset):
     return [f.name for f in fields_for_id(field_id, dataset)]
 
 # Returns all field objects for a given title keyword
-
 def fields_by_title_keyword(keyword, dataset):
     from distutils.version import LooseVersion
     fields = list(dataset.find_fields(lambda f: keyword.lower() in f.title.lower()))
     return sorted(fields, key=lambda f: LooseVersion(f.name))
 
 # Returns all field names for a given title keyword
-
 def field_names_by_title_keyword(keyword, dataset):
     return [f.name for f in fields_by_title_keyword(keyword, dataset)]
 
 # Returns all field titles for a given title keyword
-
 def field_titles_by_title_keyword(keyword, dataset):
     return [f.title for f in fields_by_title_keyword(keyword, dataset)]
