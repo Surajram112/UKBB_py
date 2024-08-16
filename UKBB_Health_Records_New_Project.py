f56a5f71e146dc910fbc17fe0683738e0f6dbce9
Suraj Ramchand <surajnramchand@gmail.com>
2024-08-16 09:58:03 +0100

    update on baseline testing

--- UKBB_Health_Records_New_Project.py
+++ UKBB_Health_Records_New_Project.py
@@ -18,10 +18,6 @@ def run_command(command):
 run_command("curl https://raw.githubusercontent.com/Surajram112/UKBB_py/main/data_file_ids.txt > data_file_ids.txt")
 run_command("curl https://raw.githubusercontent.com/Surajram112/UKBB_py/main/cols_file_ids.txt > cols_file_ids.txt")
 
-# Load baseline table and import file to run it
-run_command("curl https://raw.githubusercontent.com/Surajram112/UKBB_py/main/new_baseline.py > new_baseline.py")
-import new_baseline
-
 # Load Generate_GRS basch script
 run_command('curl https://raw.githubusercontent.com/Surajram112/UKBB_py/main/Generate_GRS.sh > Generate_GRS.sh')
 run_command('chmod +777 Generate_GRS.sh')
@@ -70,6 +66,53 @@ def load_save_data(project_folder):
     # Load extracted tables columns lists , if force download is True then original files will be reloaded
     load_files(cols_file_ids, tables_folder)
 
+def load_baseline_data(output_file='Baseline.parquet'):
+    if os.path.exists(output_file):
+        print(f"{output_file} already exists. Skipping processing.")
+        return
+
+    def month_name_to_number(month_name):
+        month_name = month_name.strip().capitalize()
+        month_dict = {name: num for num, name in enumerate(calendar.month_name) if name}
+        return month_dict.get(month_name, 0)
+
+    # Download and load baseline table
+    subprocess.run('dx download file-GZPzVp0JkBXbqJjYZvzvkjg4 -o Baseline.csv', shell=True, check=True)
+    baseline_table = pd.read_csv('Baseline.csv')
+
+    baseline_table.columns = ['eid', 'recruit_age', 'mob', 'yob', 'sex', 'tdi', 'ethnicity', 'alcohol', 'alcohol_freq', 
+                              'former_alcohol', 'ever_smoked', 'pack_years', 'smoking_status', 'current_smoking', 
+                              'father_illness', 'mother_illness', 'overall_health', 'ever_psa', 'ever_bowel_cancer_screening', 
+                              'recent_bowel_cancer_screening', 'time_since_psa', 'diabetes_diagnosed', 'gestational_diabetes', 
+                              'age_diabetes_diagnosed', 'ins_1_year', 'cancer_diagnosed', 'chol_med', 'chol_hormone_med', 
+                              'prescription_meds', 'had_menopause', 'age_menopause', 'age_menarche', 'fluid_intelligence', 
+                              'matches_time', 'birth_weight', 'diastolic_blood_pressure', 'diastolic_blood_pressure_manual', 
+                              'systolic_blood_pressure', 'height', 'waist', 'weight', 'bmi', 'hip', 'standing_height', 'body_fat', 
+                              'heel_BMD_manual', 'heel_sound_speed', 'heel_BMD', 'glucose', 'hba1c', 'hdl_cholesterol', 
+                              'ldl_cholesterol', 'total_cholesterol', 'blood_type', 'assess_date', 'centre']
+
+    # Convert month name to number
+    baseline_table['mob'] = baseline_table['mob'].apply(lambda x: month_name_to_number(x))
+
+    # Ensure 'yob' and 'mob' columns are integers
+    baseline_table['yob'] = baseline_table['yob'].astype(int)
+    baseline_table['mob'] = baseline_table['mob'].astype(int)
+
+    # Create 'dob' column
+    baseline_table['dob'] = pd.to_datetime(baseline_table[['yob', 'mob']].assign(day=15).astype(str).agg('-'.join, axis=1))
+
+    # Convert 'assess_date' to datetime
+    baseline_table['assess_date'] = pd.to_datetime(baseline_table['assess_date'])
+
+    # Calculate 'assess_age' and 'whr' columns
+    baseline_table['assess_age'] = (baseline_table['assess_date'] - baseline_table['dob']).dt.days / 365.25
+    baseline_table['whr'] = baseline_table['waist'] / baseline_table['hip']
+
+    # Save the processed baseline table and delete csv file
+    os.remove('Baseline.csv')
+    baseline_table.to_parquet(output_file)
+    print(f"Processed baseline data saved to {output_file}")
+
 def load_files(file_ids, data_folder):
     
     # Create the output folder if it doesn't already exist
@@ -92,12 +135,12 @@ def load_files(file_ids, data_folder):
             print(f"Getting original file and saving to {data_folder}.")
             
             # Transfer the files from instance ukbb_data file to local biobank project ukbb_data file
-            run_command(f'dx upload {file_path} -o {data_folder} --overwrite')
+            run_command(f'dx upload {file_path} -o {data_folder}')
             print(f"Uploaded {file_name} back to DNAnexus Project.")
 
         # Transfer the files from efficient instance ukbb_data file to efficient local biobank project ukbb_data file if not in ukbb project folder
         if os.path.exists(file_path) and not os.path.exists(local_file_path):
-            run_command(f'dx upload {file_path} -o {data_folder} --overwrite')
+            run_command(f'dx upload {file_path} -o {data_folder}')
             print(f"Uploaded {file_name} back to DNAnexus Project.")
         else:
             print(f"{file_name} already exists in the instance, at {file_path}")
