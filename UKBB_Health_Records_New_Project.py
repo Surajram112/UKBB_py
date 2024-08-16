7a7caf6ccbada72b17a2766ba0e74661839ccf22
Suraj Ramchand <surajnramchand@gmail.com>
2024-08-16 14:01:07 +0100

    update

--- UKBB_Health_Records_New_Project.py
+++ UKBB_Health_Records_New_Project.py
@@ -169,6 +169,40 @@ def filter_by_criteria(df, criteria):
     # Apply the mask to filter the dataframe
     return df.filter(mask)
 
+def identify_and_merge_array_columns(df):
+    column_patterns = {}
+    for col in df.columns:
+        match = re.match(r'(.+) \| Instance (\d+)(?: \| Array (\d+))?$', col)
+        if match:
+            base_name, instance, array = match.groups()
+            if base_name not in column_patterns:
+                column_patterns[base_name] = set()
+            column_patterns[base_name].add((instance, array))
+
+    for base_name, instances in column_patterns.items():
+        max_instance = max(int(instance) for instance, _ in instances if instance is not None)
+        for instance in range(max_instance + 1):
+            array_columns = [f"{base_name} | Instance {instance} | Array {array}" 
+                             for _, array in instances 
+                             if _ == str(instance) and array is not None]
+            if array_columns:
+                new_column_name = f"{base_name} | Instance {instance}"
+                df = df.with_columns(
+                    pl.concat_list([pl.col(col) for col in array_columns]).alias(new_column_name)
+                )
+                df = df.drop(array_columns)
+
+    return df
+
+def search_instances(df, base_column_name, search_value):
+    columns = [col for col in df.columns if col.startswith(f"{base_column_name} | Instance")]
+    return df.filter(
+        pl.any_horizontal([
+            pl.col(col).arr.contains(search_value) 
+            for col in columns
+        ])
+    )
+
 def read_GP(codes, project_folder, filename='GP_gp_clinical', extension='.parquet', filter_column=None, filter_criteria=None):
     gp_header = ['eid', 'data_provider', 'event_dt', 'read_2', 'read_3', 'value1', 'value2', 'value3', 'dob', 'assess_date', 'event_age', 'prev']
      
@@ -535,40 +569,6 @@ def read_ICD9(codes, project_folder, diagfile='HES_hesin_diag', recordfile='HES_
 #     data = data.merge(icdo3, on='description', how='left')
     
 #     return data
-
-def identify_and_merge_array_columns(df):
-    column_patterns = {}
-    for col in df.columns:
-        match = re.match(r'(.+) \| Instance (\d+)(?: \| Array (\d+))?$', col)
-        if match:
-            base_name, instance, array = match.groups()
-            if base_name not in column_patterns:
-                column_patterns[base_name] = set()
-            column_patterns[base_name].add((instance, array))
-
-    for base_name, instances in column_patterns.items():
-        max_instance = max(int(instance) for instance, _ in instances if instance is not None)
-        for instance in range(max_instance + 1):
-            array_columns = [f"{base_name} | Instance {instance} | Array {array}" 
-                             for _, array in instances 
-                             if _ == str(instance) and array is not None]
-            if array_columns:
-                new_column_name = f"{base_name} | Instance {instance}"
-                df = df.with_columns(
-                    pl.concat_list([pl.col(col) for col in array_columns]).alias(new_column_name)
-                )
-                df = df.drop(array_columns)
-
-    return df
-
-def search_instances(df, base_column_name, search_value):
-    columns = [col for col in df.columns if col.startswith(f"{base_column_name} | Instance")]
-    return df.filter(
-        pl.any_horizontal([
-            pl.col(col).arr.contains(search_value) 
-            for col in columns
-        ])
-    )
 
 def read_selfreport_illness(codes, project_folder, filename='selfreport_participant', coding_file='coding6.tsv', extension='.parquet', filter_column=None, filter_criteria=None):
     # Set up local dir for ukbb data
